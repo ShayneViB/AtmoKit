1dff6bd2b369b076571291ed6543a9d210c1ac1f
xieyang <xieyang@3clear.com>
2026-05-10 20:45:09 +0800

    删除多余文件

--- read_excel.cjs
+++
@@ -1,6 +0,0 @@
-const XLSX = require('xlsx');
-const workbook = XLSX.readFile('templates/城市监测数据小时值_2026-05-08 01_00_2026-05-08 15_00_原始（实况）.xlsx');
-const sheetName = workbook.SheetNames[0];
-const worksheet = workbook.Sheets[sheetName];
-const json = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-console.log(JSON.stringify(json.slice(0, 2), null, 2));
